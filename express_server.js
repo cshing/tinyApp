397b411f8ba54521a8d0963afba7cd2175ea530d
Carmen Shing <carmenshing@gmail.com>
2018-06-12 23:19:52 +0000

    added urls_index.ejs with url list and link for shortening

--- express_server.js
+++ express_server.js
@@ -21,7 +21,7 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("urls", (req, res) => {
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
